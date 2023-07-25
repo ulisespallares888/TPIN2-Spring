d8905d547ff8bed6361c31042738bc9519980d9e
ulisespallares888 <ulisespallares8@gmail.com>
2023-07-24 21:18:33 -0300

    COM2-[ADD]-Create project structure

---
+++ TPIN2-Spring/src/main/java/com/TPIN2Spring/TPIN2Spring/Tpin2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.TPIN2Spring.TPIN2Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpin2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpin2SpringApplication.class, args);
+	}
+
+}
---
+++ TPIN2-Spring/src/test/java/com/TPIN2Spring/TPIN2Spring/Tpin2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPIN2Spring.TPIN2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpin2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
