be46caf42d1506187bef7cb6edfd7c57c7d7d62b
ulisespallares888 <ulisespallares8@gmail.com>
2023-07-25 19:18:42 -0300

    COM4-[ADD]-Add readme

--- TPIN2-Spring/src/main/java/com/GameDev/TaskManager/GameDevApplication.java
+++ TPIN2-Spring/src/main/java/com/GameDev/TaskManager/GameDevApplication.java
@@ -1,13 +1,13 @@
-package com.TPIN2Spring.TPIN2Spring;
+package com.GameDev.TaskManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tpin2SpringApplication {
+public class GameDevApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Tpin2SpringApplication.class, args);
+		SpringApplication.run(GameDevApplication.class, args);
 	}
 
 }
